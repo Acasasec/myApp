672d6110530a684f21b858ce2452f894932b664d
Acasasec <acasasec18dw@ikzubirimanteo.com>
2019-05-26 23:17:22 +0200

    app Finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'formacion', loadChildren: './formacion/formacion.module#FormacionPageModule' }
+  { path: 'formacion', loadChildren: './formacion/formacion.module#FormacionPageModule' },  { path: 'experiencia', loadChildren: './experiencia/experiencia.module#ExperienciaPageModule' },
+  { path: 'idiomas', loadChildren: './idiomas/idiomas.module#IdiomasPageModule' }
+
 ];
 @NgModule({
   imports: [
